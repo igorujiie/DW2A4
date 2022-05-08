3d006f2c1be079e17df62d1b9cd55a6bc841b2ce
igor ujiie <igorkazuhico@hotmail.com>
2022-05-07 23:01:04 -0300

    subindo ultims versao, porem ainda com erro no userEffect no setRepositories

--- Atividades/A8/src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositotyItem(props) {
-    return (
-        <li>
-            <strong>{props.repository?.name ?? 'Repositorio Padrão'}</strong>
-            <p>{props.repository?.description}</p>
-            <a href="{props.repository?.html_url}">Acesso ao repositorio do Igor</a>
-        </li>
-    )
-}

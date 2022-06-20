0f513f7853455a43c8cf9d4780f8658670458242
igor ujiie <igorkazuhico@hotmail.com>
2022-06-20 18:03:19 -0300

    up

--- Atividades/A5/src/js/main.js
+++ Atividades/A5/src/js/main.js
@@ -1,39 +1,88 @@
-import { mask } from "./modules/mask.js";
-import { valid } from "./modules/valid.js";
-import { findUF } from "./modules/findUF.js";
-import { results } from "./modules/results.js";
-import { renderData } from "./modules/renderData.js";
+import Mascara from "./modules/masks.js";
+import Validar from "./modules/valid.js";
 
-document.querySelectorAll('input').forEach(($input) => {
-  const field = $input.dataset.js;
+const fields = document.querySelectorAll("input");
+const submitButton = document.querySelector("#button");
+let validated = false;
 
-  if (field in mask) {
-    $input.addEventListener('input', (e) => {
-      e.target.value = mask[field](e.target.value);
-    }, false);
+class data {
+  constructor(nome, cpf, dataNascimento, email, telefone, cep) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.dataNascimento = dataNascimento;
+    this.email = email;
+    this.telefone = telefone;
+    this.cep = cep;
   }
+}
 
-  $input.addEventListener('input', (e) => {
-    if (valid[e.target.id](e.target.value)) {
-      e.target.classList.remove("errorInput");
-    } else {
-      e.target.classList.add("errorInput");
-    };
-  }, false);
+//mascarar inputs
+fields.forEach((input) => {
+  const field = input.dataset.js;
+
+  //no caso do email
+  if (field != undefined) {
+    input.addEventListener(
+      "input",
+      (e) => {
+        e.target.value = Mascara[field](e.target.value);
+      },
+      false
+    );
+  }
 });
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', async el => {
-  el.preventDefault();
-  try {
-    const uf = await findUF();
-    const data = await results(uf);
-    if (data[0]) {
-      renderData(data); 
-    } else {
-      document.querySelector('#cep').classList.add("errorInput");
-    }
-  } catch (err) {
-    console.log(err);
+function validarCPF() {
+  //utilização de uma expressão regular para pegar somente os números
+  const cpfValue = fields[1].value.replace(/[^0-9]/g, "");
+
+  if (Validar.cpf(cpfValue) === -1) {
+    fields[1].classList = "errorInput";
+  } else {
+    fields[1].classList = "";
+    console.log("CPF é válido!");
   }
+}
+
+function validarDataNascimento() {
+  if (Validar.date(fields[2].value) === -1) {
+    fields[2].classList = "errorInput";
+  } else {
+    fields[2].classList = "";
+    console.log("Data de nascimento válido!");
+  }
+}
+
+function validarEmail() {
+  if (Validar.email(fields[3].value) == -1) {
+    fields[3].classList = "errorInput";
+  } else {
+    fields[3].classList = "";
+    console.log("E-mail é válido!");
+  }
+}
+
+function validarTelefone() {
+  const phoneValue = fields[4].value.replace(/[^0-9]/g, "");
+
+  if (Validar.telefone(phoneValue) === -1) {
+    fields[4].classList = "errorInput";
+  } else {
+    fields[4].classList = "";
+    console.log("Telefone válido!");
+  }
+}
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  validarCPF();
+  validarDataNascimento();
+  validarEmail();
+  validarTelefone();
+
+  validated = false;
+  fields.forEach((tag) => {
+    if (tag.classList.contains("errorInput")) validated = true;
+  });
+  if (!validated) alert("Campos validados com sucesso!");
 });
--- Atividades/A5/src/js/modules/valid.js
+++ Atividades/A5/src/js/modules/valid.js
@@ -1,8 +1,111 @@
-export const valid = {
-  cep(value) {
-    if (value == '' || value == ' ' || value.length < 9) {
-      return false;
+// As validações retornam -1 caso o valor seja inválido
+const validar = {
+  cpf(value, controlador = 9) {
+    let verificadorMomento = [];
+    let auxiliar = [];
+    let produtoEntreArrays = [];
+    let somatorioDosProdutos = 0;
+    let restoDaDivisao;
+    let result = 0;
+
+    //separa os dígitos em 9 e dps em 10
+    //passar 9 no controlador para a 1º vez
+    function capturarNumero(value) {
+      for (let i = 0; i < controlador; i++)
+        verificadorMomento[i] = value.substring(i, i + 1);
     }
-    return true;
-  }
-}
+
+    //pegar o auxiliar de acordo com qual dígito verificador é no momento
+    function atribuirValoresAoAuxiliar() {
+      if (controlador == 9)
+        for (let i = 10, index = 0; index <= controlador - 1; i--, index++) {
+          auxiliar[index] = i;
+        }
+      else if (controlador == 10) {
+        for (let i = 11, index = 0; index <= controlador - 1; i--, index++) {
+          auxiliar[index] = i;
+        }
+      }
+    }
+
+    //multiplicar os arrays e colocar para somar
+    function operacoesComOsArrays() {
+      for (let i = 0; i <= controlador - 1; i++) {
+        produtoEntreArrays[i] = verificadorMomento[i] * auxiliar[i];
+        somatorioDosProdutos += produtoEntreArrays[i];
+      }
+    }
+
+    function verificarIgualdadeDoDigitoVerificador() {
+      restoDaDivisao = (somatorioDosProdutos *= 10) % 11;
+
+      if (restoDaDivisao == 10 || restoDaDivisao == 11) restoDaDivisao = 0;
+
+      //sei que é a primeira vez
+      if (controlador === 9) {
+        if (restoDaDivisao == value.substring(9, 10)) {
+          //utilizando recursividade
+          result = validar.cpf(value, 10);
+        } else {
+          //indica cpf inválido
+          result = -1;
+        }
+      }
+
+      //sei que é a segunda vez
+      else if (controlador === 10) {
+        if (restoDaDivisao != value.substring(10, 11))
+          //indica cpf inválido
+          result = -1;
+      }
+    }
+
+    //caso não seja inserido nenhum valor ao cpf
+    if (!(value === "")) {
+      capturarNumero(value);
+      atribuirValoresAoAuxiliar();
+      operacoesComOsArrays();
+      verificarIgualdadeDoDigitoVerificador();
+    } else {
+      result = -1;
+    }
+
+    return result;
+  },
+
+  date(date) {
+    const today = new Date();
+    date = date.split("/");
+
+    const day = Number(date[0]);
+    const month = Number(date[1]);
+    const year = Number(date[2]);
+
+    if (year === today.getFullYear()) {
+      if (month <= (today.getMonth() + 1)) {
+        if (day <= today.getDate()) {
+          return 0;
+        }
+      }
+    }
+
+      if (year < today.getFullYear()) {
+        if ((month <= 12 )) {
+          if (day <= 31) {
+            return 0;
+          }
+        }
+      } 
+      return -1;
+  },
+
+  email(value) {
+    if (!(value.includes("@") && value.includes("."))) return -1;
+  },
+
+  telefone(value) {
+    if (value < 8) return -1;
+  },
+};
+
+export default validar;
